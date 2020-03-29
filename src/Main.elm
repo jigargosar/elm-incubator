a2b62b299a6b4b1beac5c1e72e53dd8bcff3b0d7
Jigar Gosar <jigar.gosar@gmail.com>
2020-03-29 14:11:55 +0530

    main scaffolding

--- src/Main.elm
+++ src/Main.elm
@@ -1,5 +1,68 @@
 module Main exposing (main)
 
-import Html exposing (text)
+-- Browser.Element Scaffold
 
-main = text "hi"
+import Browser
+import Html exposing (Html, text)
+
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view _ =
+    text "Hi"
+
+
+
+-- Main
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
