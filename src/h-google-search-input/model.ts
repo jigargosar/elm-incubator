286efdc7a2d08adee9633a5c4fe68f08758ef762
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-02 19:04:48 +0530

    hyperapp google-search-input.js

--- src/h-google-search-input/model.ts
+++ src/h-google-search-input/model.ts
@@ -1,3 +1,15 @@
+type SearchWidget = ['SW', string, InputValue, Suggestions]
+
+type InputValue = ['TYPED', string] | ['OVERRIDDEN', string, string]
+
+type Suggestions =
+  | ['HIDDEN', NEL<string>]
+  | ['VISIBLE', NeSelection<string>]
+
+type NeSelection<a> = ['NONE_SELECTED', NEL<a>] | ['SELECTED', LCR<a>]
+
+type LCR<a> = ['LCR', a[], a, a[]]
+
 type NEL<a> = ['NEL', a, a[]]
 
 export function initNEL<A>(h: A, t: A[]): NEL<A> {
