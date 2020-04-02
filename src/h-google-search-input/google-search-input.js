815a85007e2ea0a9cc23e35577b71dc31fd30654
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-02 17:35:20 +0530

    hyperapp google-search-input.js

--- src/h-google-search-input/google-search-input.js
+++ src/h-google-search-input/google-search-input.js
@@ -14,7 +14,7 @@ function initState() {
 function view(state) {
   return div({ class: 'pv3 measure center f4 lh-title' }, [
     div({ tabindex: 0 }, ['before input']),
-    (viewInput2('foo bar')),
+    viewInput2('foo bar'),
     div({ tabindex: 0 }, ['after input']),
   ])
 }
