002c90ffcc6046a46e3c4f6522b76dfc277229a9
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-02 17:34:07 +0530

    hyperapp google-search-input.js

--- src/h-google-search-input/google-search-input.js
+++ src/h-google-search-input/google-search-input.js
@@ -11,16 +11,20 @@ function initState() {
 
 // VIEW
 
+function viewInput2() {
+  return div({ class: 'flex ba br4 b--moon-gray br--top shadow-1' }, [
+    input({
+      class: 'ph3 pv2 flex-auto bn bg-transparent outline-0',
+      value: 'foo bar',
+      autofocus: true,
+    }),
+  ])
+}
+
 function view(state) {
   return div({ class: 'pv3 measure center f4 lh-title' }, [
     div({ tabindex: 0 }, ['before input']),
-    div({ class: 'flex ba br4 b--moon-gray br--top shadow-1' }, [
-      input({
-        class: 'ph3 pv2 flex-auto bn bg-transparent outline-0',
-        value: 'foo bar',
-        autofocus: true,
-      }),
-    ]),
+    viewInput2(),
     div({ tabindex: 0 }, ['after input']),
   ])
 }
