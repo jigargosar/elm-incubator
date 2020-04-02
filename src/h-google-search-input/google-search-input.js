512c412a85f9be5664e8010012418d57aa1e1745
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-02 17:29:40 +0530

    hyperapp google-search-input.js

---
+++ src/h-google-search-input/google-search-input.js
@@ -0,0 +1,51 @@
+import { h, app } from 'hyperapp'
+import 'tachyons'
+
+// MODEL
+
+function initState() {
+  return { ct: 0 }
+}
+
+// UPDATE
+
+// VIEW
+
+function view(state) {
+  return div({ class: 'pv3 measure center f4 lh-title' }, [
+    div({tabindex:0}, ['before input']),
+    div({ class: 'flex' }, [
+      input({
+        class:
+          'ph3 pv2 flex-auto ba br4 b--moon-gray br--top shadow-hover shadow-0 outline-0',
+        value: 'foo bar',
+      }),
+    ]),
+    div({tabindex:0}, ['after input']),
+  ])
+}
+
+function text(string) {
+  return `${string}`
+}
+
+function div(...args) {
+  return h('div', ...args)
+}
+
+function button(...args) {
+  return h('button', ...args)
+}
+function input(options) {
+  return h('input', options)
+}
+
+// MAIN
+
+{
+  app({
+    init: initState(),
+    view: view,
+    node: document.getElementById('app'),
+  })
+}
