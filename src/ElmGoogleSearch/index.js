3693a099a0d608301e8bb5a19472f3c7c3a96f20
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-03 17:33:23 +0530

    switch experiment: to elm-seeds game

--- src/ElmGoogleSearch/index.js
+++ src/ElmGoogleSearch/index.js
@@ -1,24 +1,8 @@
-import { pathOr } from 'ramda'
 const Module = require('./Main.elm')
 
 require('./styles.css')
 require('tachyons')
 
-{
-  Module['Elm']['ElmGoogleSearch']['Main'].init({
-    node: document.getElementById('root'),
-  })
-}
-
-function initElmModuleWithPortHelpers(initParams, main) {
-  const app = main.init(initParams)
-  function subscribe(portName, callback) {
-    pathOr(() => console.error(`${portName}.subscribe Port Not Found`), [
-      'ports',
-      portName,
-      'subscribe',
-    ])(app)(callback)
-  }
-
-  return [app, subscribe]
-}
+Module['Elm']['ElmGoogleSearch']['Main'].init({
+  node: document.getElementById('root'),
+})
