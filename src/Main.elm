28ed495833c7de5e9d8ce09cae20499673743c91
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-04 17:09:12 +0530

    simulate elm-seed board

--- src/Main.elm
+++ src/Main.elm
@@ -157,41 +157,33 @@ view (M cx com) =
             List.range 0 (gw - 1)
                 |> List.concatMap (\x -> List.range 0 (gh - 1) |> List.map (drawCell x))
 
-        svgView =
-            [ draw <|
-                group
-                    [ rectangle "rgba(153, 248, 255)" swPx shPx
-                    , rectangle "lightyellow" (toFloat (gw + 1) * gcwPx) (toFloat (gh + 1) * gcwPx)
-                    , group gridCellsView
-                        |> move
-                            (((toFloat gw * gcwPx) - gcwPx) * -0.5)
-                            (((toFloat gh * gcwPx) - gcwPx) * -0.5)
-                    , group
-                        [ ellipse "black" 1 10
-                        , ellipse "black" 10 1
-                        ]
-                        |> move mx my
-                    ]
+        drawing =
+            [ rectangle "rgba(153, 248, 255)" swPx shPx
+            , rectangle "lightyellow" (toFloat (gw + 1) * gcwPx) (toFloat (gh + 1) * gcwPx)
+            , group gridCellsView
+                |> move
+                    (((toFloat gw * gcwPx) - gcwPx) * -0.5)
+                    (((toFloat gh * gcwPx) - gcwPx) * -0.5)
+            , group
+                [ ellipse "black" 1 10
+                , ellipse "black" 10 1
+                ]
+                |> move mx my
             ]
     in
     div
         [ class "fixed absolute--fill flex"
         , SE.on "mousemove" pageMouseMoveDecoder
         ]
-        [ svgCanvas svgView
-        ]
-
-
-svgCanvas : List HM -> HM
-svgCanvas children =
-    Svg.svg
-        [ TA.viewBox (swPx * -0.5) (shPx * -0.5) swPx shPx
-        , TA.class [ "flex-auto" ]
-        , TA.preserveAspectRatio (TT.Align TT.ScaleMid TT.ScaleMid) TT.Meet
-        , style "background-color" "rgba(183, 169, 255)"
-        ]
-        [ Svg.g [ SA.id "canvas" ]
-            (draw (rectangle "none" swPx shPx) :: children)
+        [ Svg.svg
+            [ TA.viewBox (swPx * -0.5) (shPx * -0.5) swPx shPx
+            , TA.class [ "flex-auto" ]
+            , TA.preserveAspectRatio (TT.Align TT.ScaleMid TT.ScaleMid) TT.Meet
+            , style "background-color" "rgba(183, 169, 255)"
+            ]
+            [ Svg.g [ SA.id "canvas" ]
+                (List.map draw (rectangle "none" swPx shPx :: drawing))
+            ]
         ]
 
 
