30fc572f19e8e278302c8f4410fc0962edf57b17
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-09 18:51:04 +0530

    seed-game-v4: with animationFrame

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-const Module = require('./Main3.elm')
+const Module = require('./Main4.elm')
 
 require('./styles.css')
 require('tachyons')
 
-Module['Elm']['Main3'].init({
+Module['Elm']['Main4'].init({
   node: document.getElementById('root'),
   flags: { now: Date.now(), bs: [window.innerWidth, window.innerHeight] },
 })
