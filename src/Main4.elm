f389ef68ae5a7fa62627823b2239173753bed08c
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-10 15:49:30 +0530

    seed-game-v4: with animationFrame

--- src/Main4.elm
+++ src/Main4.elm
@@ -56,52 +56,6 @@ gridIndices =
 
 
 
--- ANIM
-
-
-type alias Anim =
-    { from : Float, to : Float, duration : Float, elapsed : Float }
-
-
-initAnim : Float -> Float -> Anim
-initAnim from to =
-    { from = from, to = to, duration = 200, elapsed = 0 }
-
-
-animReverse : Anim -> Anim
-animReverse ({ from, to, duration, elapsed } as anim) =
-    { anim | from = to, to = from, elapsed = duration - elapsed }
-
-
-animProgress : Anim -> Float
-animProgress { from, to, duration, elapsed } =
-    if from == to || duration <= 0 || elapsed >= duration then
-        1
-
-    else
-        elapsed / duration
-
-
-animIsDone : Anim -> Bool
-animIsDone anim =
-    animProgress anim == 1
-
-
-animValue : Anim -> Float
-animValue ({ from, to, duration, elapsed } as anim) =
-    (to - from) * animProgress anim + from
-
-
-animTick : Float -> Anim -> Anim
-animTick delta ({ elapsed } as anim) =
-    if animIsDone anim then
-        anim
-
-    else
-        { anim | elapsed = elapsed + delta }
-
-
-
 -- Model
 
 
@@ -469,3 +423,49 @@ renderTransform m =
 
 opacity =
     TypedSvg.Attributes.opacity << Opacity
+
+
+
+-- ANIM
+
+
+type alias Anim =
+    { from : Float, to : Float, duration : Float, elapsed : Float }
+
+
+initAnim : Float -> Float -> Anim
+initAnim from to =
+    { from = from, to = to, duration = 200, elapsed = 0 }
+
+
+animReverse : Anim -> Anim
+animReverse ({ from, to, duration, elapsed } as anim) =
+    { anim | from = to, to = from, elapsed = duration - elapsed }
+
+
+animProgress : Anim -> Float
+animProgress { from, to, duration, elapsed } =
+    if from == to || duration <= 0 || elapsed >= duration then
+        1
+
+    else
+        elapsed / duration
+
+
+animIsDone : Anim -> Bool
+animIsDone anim =
+    animProgress anim == 1
+
+
+animValue : Anim -> Float
+animValue ({ from, to, duration, elapsed } as anim) =
+    (to - from) * animProgress anim + from
+
+
+animTick : Float -> Anim -> Anim
+animTick delta ({ elapsed } as anim) =
+    if animIsDone anim then
+        anim
+
+    else
+        { anim | elapsed = elapsed + delta }
