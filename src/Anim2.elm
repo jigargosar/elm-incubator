6e4f15980a73807cb42dbe23732a6f3c90246b2b
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-10 16:02:39 +0530

    seed-game-v4: extract Anim, for replacing it with Anim2

--- src/Anim2.elm
+++ src/Anim2.elm
@@ -1,16 +1,16 @@
-module Anim2 exposing (Anim, animReverse, animTick, initAnim)
+module Anim2 exposing (Anim2, animReverse, animTick, initAnim)
 
 import Anim
 import Animation as A
 
 
-type Anim
-    = Anim Float A.Animation
+type Anim2
+    = Anim2 Float A.Animation
 
 
-initAnim : Float -> Float -> Anim
+initAnim : Float -> Float -> Anim2
 initAnim from to =
-    Anim 0
+    Anim2 0
         (A.animation 0
             |> A.from from
             |> A.to to
@@ -18,11 +18,11 @@ initAnim from to =
         )
 
 
-animReverse : Anim -> Anim
-animReverse (Anim c a) =
-    A.undo c a |> Anim c
+animReverse : Anim2 -> Anim2
+animReverse (Anim2 c a) =
+    A.undo c a |> Anim2 c
 
 
-animTick : Float -> Anim -> Anim
-animTick d (Anim c a) =
-    Anim (c + d) a
+animTick : Float -> Anim2 -> Anim2
+animTick d (Anim2 c a) =
+    Anim2 (c + d) a
