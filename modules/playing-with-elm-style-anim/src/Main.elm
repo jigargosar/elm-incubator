ffb3a76a17b032d727dce6eccab68bc9cbc0d96d
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-11 21:02:55 +0530

    using parent webpack config for nested module

--- modules/playing-with-elm-style-anim/src/Main.elm
+++ modules/playing-with-elm-style-anim/src/Main.elm
@@ -1,7 +1,66 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser
+import Html exposing (Html, text)
 
 
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view _ =
+    text "HW"
+
+
+
+-- Main
+
+
+main : Program Flags Model Msg
 main =
-    text "h4"
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
