03aa328ad2a5d5edb79a22b938524fc05c2960d8
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-12 18:14:41 +0530

    playing with elm-animator: MultiValue.elm

---
+++ modules/playing-with-animator/src/MultiValue.elm
@@ -0,0 +1,71 @@
+module MultiValue exposing (main)
+
+import Browser
+import Html exposing (Html)
+
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view _ =
+    empty
+
+
+empty : Html msg
+empty =
+    Html.text ""
+
+
+
+-- Main
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
