9672e36076acc75f185970338c686d3262792922
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-25 17:47:00 +0530

    modeling seeds game

--- modules/seeds-game/src/GameRunner.elm
+++ modules/seeds-game/src/GameRunner.elm
@@ -5,7 +5,7 @@ import Browser exposing (Document)
 import Dict
 import GameModel as Game
 import Grid exposing (GI, Grid)
-import Html exposing (Html, button, div, node, table, text)
+import Html exposing (Html, button, node, table, text)
 import Html.Attributes exposing (autofocus, class, style)
 import Html.Events exposing (onClick)
 import List.Extra
