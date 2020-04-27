8352cff6d996e20c0e2b3f6e763692750bfac489
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-27 17:56:48 +0530

    nature of code: random walker

--- modules/random-walker/src/Main.elm
+++ modules/random-walker/src/Main.elm
@@ -1,5 +1,71 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser exposing (Document)
+import Html exposing (Html, text)
 
-main = text "hi"
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init () =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+
+-- View
+
+
+type alias DM =
+    Document Msg
+
+
+view : Model -> DM
+view _ =
+    Document "Main"
+        [ text "Hello Main" ]
+
+
+
+-- Main
+
+
+main : Program Flags Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
