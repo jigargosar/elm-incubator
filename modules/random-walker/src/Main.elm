7b64d7dd1278c30b56537417d2aecd5e1c9b3ad6
Jigar Gosar <jigar.gosar@gmail.com>
2020-04-28 19:46:39 +0530

    nature of code: random walker

--- modules/random-walker/src/Main.elm
+++ modules/random-walker/src/Main.elm
@@ -5,15 +5,12 @@ import Browser exposing (Document)
 import Browser.Events
 import Dict exposing (Dict)
 import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
 import Random exposing (Generator)
-import Random.Extra as Random
 import Svg
 import Svg.Attributes
 import Svg.Keyed
 import TypedSvg.Attributes
 import TypedSvg.Attributes.InPx
-import TypedSvg.Types exposing (Opacity(..))
 
 
 
@@ -57,6 +54,17 @@ walk size (RandomWalker last fd) =
         |> Random.map func
 
 
+nextPointGenerator : Size -> Point -> Random.Generator Point
+nextPointGenerator size point =
+    Random.map2
+        (\func by ->
+            func by point
+                |> constrainPointInSize size
+        )
+        (Random.uniform mapX [ mapY ])
+        (Random.uniform inc [ dec ])
+
+
 walkSteps : Size -> Int -> RandomWalker -> Generator RandomWalker
 walkSteps size steps rw =
     List.range 0 (steps - 1)
@@ -68,9 +76,7 @@ walkSteps size steps rw =
 
 
 type alias Model =
-    { freqDict : FreqDict
-    , last : Point
-    , size : Size
+    { size : Size
     , walker : RandomWalker
     , seed : Random.Seed
     }
@@ -91,9 +97,7 @@ init () =
                 |> sizeMidPoint
                 |> roundPoint
     in
-    ( { freqDict = freqDictSingleton start
-      , last = start
-      , size = size
+    ( { size = size
       , walker = initRandomWalker start
       , seed = Random.initialSeed 0
       }
@@ -103,19 +107,6 @@ init () =
 
 updateRandomWalker : Model -> Model
 updateRandomWalker model =
-    let
-        ( nextPoint, nextSeed ) =
-            Random.step (nextPointGenerator model.size model.last) model.seed
-    in
-    { model
-        | seed = nextSeed
-        , last = nextPoint
-        , freqDict = freqDictInsertPoint nextPoint model.freqDict
-    }
-
-
-updateRandomWalker2 : Model -> Model
-updateRandomWalker2 model =
     let
         ( nextRW, nextSeed ) =
             Random.step (walkSteps model.size 1 model.walker) model.seed
@@ -145,7 +136,6 @@ update message model =
         Tick ->
             model
                 |> updateRandomWalker
-                |> updateRandomWalker2
                 |> addCmd Cmd.none
 
 
@@ -170,21 +160,16 @@ type alias DM =
 
 view : Model -> DM
 view model =
-    --let
-    --    points =
-    --        Random.step (randomWalkerPointsGenerator model.size 50000) (Random.initialSeed 1)
-    --            |> Tuple.first
-    --
-    --    freqDict =
-    --        points |> pointsToFrequencyDict
-    --in
     Document "Main"
         [ div [] [ text "Hello Main" ]
         , div []
-            [ viewFreqDict model.size model.freqDict
-            , viewRandomWalker2 model.size model.walker
+            [ viewRandomWalker model.size model.walker
             ]
         ]
+
+
+viewRandomWalker size (RandomWalker _ fd) =
+    viewFreqDict size fd
 
 
 viewFreqDict size freqDict =
@@ -197,59 +182,16 @@ viewFreqDict size freqDict =
         ]
 
 
-viewRandomWalker2 size (RandomWalker _ fd) =
-    viewFreqDict size fd
-
-
-
---pointsToFrequencyDict : List Point -> FreqDict
---pointsToFrequencyDict =
---    List.map pointToTuple >> Dict.Extra.frequencies
-
-
 renderFrequencyDict : FreqDict -> Svg.Svg msg
 renderFrequencyDict =
     Dict.toList
         >> List.map
             (\( ( x, y ), freq ) ->
                 ( Debug.toString ( x, y )
-                , renderDot x y (toFloat freq * 0.05 |> atMost 1)
+                , renderDot x y (freq * 5 |> atMost 100)
                 )
             )
         >> Svg.Keyed.node "g" []
-
-
-
---randomWalkerPointsGenerator : Size -> Int -> Random.Generator (List Point)
---randomWalkerPointsGenerator size len =
---    let
---        func2 _ ( ( h, t ), seed0 ) =
---            Random.step (nextPointGenerator size h) seed0
---                |> Tuple.mapFirst (\np -> ( np, h :: t ))
---
---        func startPoint seed =
---            List.range 0 (len - 1)
---                |> List.foldl func2 ( ( startPoint, [] ), seed )
---                |> Tuple.first
---                |> consToList
---                |> List.reverse
---    in
---    Random.map2 func
---        (randomPointGenerator size)
---        Random.independentSeed
---consToList ( h, t ) =
---    h :: t
-
-
-nextPointGenerator : Size -> Point -> Random.Generator Point
-nextPointGenerator size point =
-    Random.map2
-        (\func by ->
-            func by point
-                |> constrainPointInSize size
-        )
-        (Random.uniform mapX [ mapY ])
-        (Random.uniform inc [ dec ])
 
 
 
@@ -266,17 +208,18 @@ renderDot x y o =
         , TypedSvg.Attributes.InPx.y y
         , TypedSvg.Attributes.InPx.width dotWidth
         , TypedSvg.Attributes.InPx.height dotWidth
-        , TypedSvg.Attributes.opacity (Opacity o)
+        , Svg.Attributes.opacity (String.fromInt o ++ "%")
         ]
         []
 
 
 
--- Random
---random : Random.Generator Float
---random =
---    Random.float 0 1
 -- Basics
+
+
+roundFloat : Float -> Float
+roundFloat =
+    round >> toFloat
 
 
 inc =
@@ -326,28 +269,6 @@ mapY func point =
 constrainPointInSize : Size -> Point -> Point
 constrainPointInSize size point =
     Point (clamp 0 (size.width - 1) point.x) (clamp 0 (size.height - 1) point.y)
-
-
-
---randomPointGenerator : Size -> Random.Generator Point
---randomPointGenerator size =
---    Random.map2
---        (\x y ->
---            newPoint
---                ((x * (size.width - 1))
---                    |> roundFloat
---                )
---                ((y * (size.height - 1))
---                    |> roundFloat
---                )
---        )
---        random
---        random
-
-
-roundFloat : Float -> Float
-roundFloat =
-    round >> toFloat
 
 
 
