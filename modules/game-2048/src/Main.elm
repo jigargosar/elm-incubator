5de2d345fb2ae0fc0348d76f0e5c948687cb2996
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-04 17:59:35 +0530

    the coding train: 2048

--- modules/game-2048/src/Main.elm
+++ modules/game-2048/src/Main.elm
@@ -240,10 +240,10 @@ initialNamedGridList =
          --, SlideDown
         ]
         (initBoard (Random.initialSeed 0)
-            [ [ 2, 0, 0, 0 ]
-            , [ 2, 4, 4, 4 ]
-            , [ 0, 0, 0, 0 ]
-            , [ 0, 0, 0, 0 ]
+            [ [ 0, 2, 2, 0 ]
+            , [ 2, 4, 2, 2 ]
+            , [ 2, 2, 4, 2 ]
+            , [ 0, 2, 2, 0 ]
             ]
         )
 
