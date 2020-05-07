c607c42b9aa0deb5d12dec545fa64512ca4d1f14
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-07 20:27:07 +0530

    the coding train: 2048

--- modules/game-2048/src/TileGrid.elm
+++ modules/game-2048/src/TileGrid.elm
@@ -1,8 +1,10 @@
 module TileGrid exposing (TileGrid, fromNumRows)
 
-import Basics.Extra exposing (swap, uncurry)
+import Basics.Extra exposing (flip, swap, uncurry)
+import Dict
 import Grid
 import List.Extra
+import Random
 
 
 type TileGrid
@@ -30,6 +32,13 @@ fromNumRows lists =
             initCellIdGenerator
         |> Tuple.mapSecond (Grid.fromRowLists size EmptyCell)
         |> uncurry TileGrid
+
+
+type SlideMsg
+    = SlideUp
+    | SlideDown
+    | SlideLeft
+    | SlideRight
 
 
 
@@ -75,15 +84,139 @@ newCell num kind =
 
 
 
--- CellEntry
+-- CellGrid
+
+
+type alias CellGrid =
+    Grid.Grid Cell
 
 
 type alias CellEntry =
     Grid.Entry Cell
 
 
-cellEntriesCompactRight : CellIdGenerator -> List CellEntry -> ( ( Int, CellIdGenerator ), List CellEntry )
-cellEntriesCompactRight idGen entries =
+cellGridUpdate :
+    SlideMsg
+    -> CellIdGenerator
+    -> CellGrid
+    -> Maybe (Random.Generator ( ( Int, Grid.Pos, CellIdGenerator ), CellGrid ))
+cellGridUpdate message idGen0 oldGrid =
+    let
+        ( ( score, idGen1 ), newGrid ) =
+            cellGridSlide message idGen0 oldGrid
+    in
+    if newGrid /= oldGrid then
+        cellGridFillRandom idGen1 newGrid
+            |> Maybe.map (Random.map (\( ( p, idGen ), g ) -> ( ( score, p, idGen ), g )))
+
+    else
+        Nothing
+
+
+
+-- FILL
+
+
+cellGridFillRandom : CellIdGenerator -> CellGrid -> Maybe (Random.Generator ( ( Grid.Pos, CellIdGenerator ), CellGrid ))
+cellGridFillRandom idGen0 grid =
+    let
+        func ( pos, num ) =
+            let
+                ( cell, idGen ) =
+                    newCell num GeneratedCell idGen0
+            in
+            case Grid.set pos cell grid of
+                Nothing ->
+                    Debug.todo "This should never happen"
+
+                Just filledGrid ->
+                    ( ( pos, idGen ), filledGrid )
+    in
+    cellGridEmptyPositionsCons grid
+        |> Maybe.map (cellEntryGenerator >> Random.map func)
+
+
+cellGridEmptyPositionsCons : CellGrid -> Maybe (Cons Grid.Pos)
+cellGridEmptyPositionsCons grid =
+    Grid.toDict grid
+        |> Dict.filter (\_ v -> v == EmptyCell)
+        |> Dict.keys
+        |> consFromList
+
+
+cellEntryGenerator : Cons Grid.Pos -> Random.Generator ( Grid.Pos, Int )
+cellEntryGenerator ( pos, posList ) =
+    Random.pair
+        (Random.uniform pos posList)
+        (Random.uniform 2 [ 4 ])
+
+
+
+-- Cons
+
+
+type alias Cons a =
+    ( a, List a )
+
+
+consFromList : List a -> Maybe (Cons a)
+consFromList list =
+    case list of
+        [] ->
+            Nothing
+
+        h :: t ->
+            Just ( h, t )
+
+
+
+-- FILL END
+
+
+cellGridSlide : SlideMsg -> CellIdGenerator -> CellGrid -> ( ( Int, CellIdGenerator ), CellGrid )
+cellGridSlide message =
+    case message of
+        SlideUp ->
+            cellGridCompactUp
+
+        SlideDown ->
+            cellGridCompactDown
+
+        SlideLeft ->
+            cellGridCompactLeft
+
+        SlideRight ->
+            cellGridCompactRight
+
+
+cellGridCompactRight : CellIdGenerator -> CellGrid -> ( ( Int, CellIdGenerator ), CellGrid )
+cellGridCompactRight idGen =
+    cellGridCompactHelp idGen Grid.toRowEntries
+
+
+cellGridCompactLeft : CellIdGenerator -> CellGrid -> ( ( Int, CellIdGenerator ), CellGrid )
+cellGridCompactLeft idGen =
+    Grid.reverseRows >> cellGridCompactHelp idGen Grid.toRowEntries >> Tuple.mapSecond Grid.reverseRows
+
+
+cellGridCompactDown : CellIdGenerator -> CellGrid -> ( ( Int, CellIdGenerator ), CellGrid )
+cellGridCompactDown idGen =
+    cellGridCompactHelp idGen Grid.toColumnEntries
+
+
+cellGridCompactUp : CellIdGenerator -> CellGrid -> ( ( Int, CellIdGenerator ), CellGrid )
+cellGridCompactUp idGen =
+    Grid.reverseColumns >> cellGridCompactHelp idGen Grid.toColumnEntries >> Tuple.mapSecond Grid.reverseColumns
+
+
+cellGridCompactHelp : CellIdGenerator -> (CellGrid -> List (List CellEntry)) -> CellGrid -> ( ( Int, CellIdGenerator ), CellGrid )
+cellGridCompactHelp idGen toCellEntryLists grid =
+    List.Extra.mapAccuml cellEntriesCompactRight ( 0, idGen ) (toCellEntryLists grid)
+        |> Tuple.mapSecond (List.concat >> flip Grid.replaceEntries grid)
+
+
+cellEntriesCompactRight : ( Int, CellIdGenerator ) -> List CellEntry -> ( ( Int, CellIdGenerator ), List CellEntry )
+cellEntriesCompactRight acc entries =
     let
         positions =
             List.map Tuple.first entries
@@ -91,7 +224,7 @@ cellEntriesCompactRight idGen entries =
         cellValues =
             List.map Tuple.second entries
     in
-    cellListCompactRight idGen cellValues
+    cellListCompactRight acc cellValues
         |> Tuple.mapSecond (List.Extra.zip positions)
 
 
@@ -104,20 +237,20 @@ type alias CellList =
 
 
 type alias CompactAcc =
-    ( Int, ( ( Maybe Cell, CellList ), CellIdGenerator ) )
+    ( ( Int, CellIdGenerator ), ( Maybe Cell, CellList ) )
 
 
-cellListCompactRight : CellIdGenerator -> CellList -> ( ( Int, CellIdGenerator ), CellList )
-cellListCompactRight idGen0 =
+cellListCompactRight : ( Int, CellIdGenerator ) -> CellList -> ( ( Int, CellIdGenerator ), CellList )
+cellListCompactRight acc0 =
     let
         func : Cell -> CompactAcc -> CompactAcc
-        func cell (( score, ( ( maybeUnprocessed, processed ), idGen ) ) as acc) =
+        func cell (( ( score, idGen ), ( maybeUnprocessed, processed ) ) as acc) =
             case ( cell, maybeUnprocessed ) of
                 ( EmptyCell, _ ) ->
                     acc
 
                 ( _, Nothing ) ->
-                    ( score, ( ( Just cell, processed ), idGen ) )
+                    ( ( score, idGen ), ( Just cell, processed ) )
 
                 ( NumCell id num _, Just ((NumCell id2 num2 _) as unprocessed) ) ->
                     if num == num2 then
@@ -131,10 +264,10 @@ cellListCompactRight idGen0 =
                             updatedScore =
                                 score + mergedNum
                         in
-                        ( updatedScore, ( ( Nothing, mergedCell :: processed ), nextIdGen ) )
+                        ( ( updatedScore, nextIdGen ), ( Nothing, mergedCell :: processed ) )
 
                     else
-                        ( score, ( ( Just cell, unprocessed :: processed ), idGen ) )
+                        ( ( score, idGen ), ( Just cell, unprocessed :: processed ) )
 
         unprocessedTupleToCellList ( maybeUnprocessed, processed ) =
             (case maybeUnprocessed of
@@ -146,14 +279,12 @@ cellListCompactRight idGen0 =
             )
                 |> cellListPadLeft
 
-        compactAccToReturn ( score, ( unprocessedTuple, idGen ) ) =
-            ( ( score
-              , idGen
-              )
+        compactAccToReturn ( acc, unprocessedTuple ) =
+            ( acc
             , unprocessedTupleToCellList unprocessedTuple
             )
     in
-    List.foldr func ( 0, ( ( Nothing, [] ), idGen0 ) )
+    List.foldr func ( acc0, ( Nothing, [] ) )
         >> compactAccToReturn
 
 
