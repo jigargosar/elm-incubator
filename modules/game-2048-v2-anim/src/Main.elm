7902aaa44d8d2d340754d4efc97ad10c26e3886d
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-08 19:25:24 +0530

    the coding train: 2048 v2 with animation

--- modules/game-2048-v2-anim/src/Main.elm
+++ modules/game-2048-v2-anim/src/Main.elm
@@ -142,7 +142,7 @@ viewTile tile =
         , style "background-color" "rgba(255,255,255,0.9)"
         , class "absolute flex justify-center items-center"
         , style "transform" (renderTileTransform tile)
-        , style "transition" "transform 500ms"
+        , style "transition" "transform 750ms"
         ]
         [ div
             [ style "width" "100px"
