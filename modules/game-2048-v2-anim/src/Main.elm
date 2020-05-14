33271d794d8ffa7209533fb3216add60a7cb7b5a
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-14 14:52:05 +0530

    view polish
    the coding train: 2048 v2 with animation

--- modules/game-2048-v2-anim/src/Main.elm
+++ modules/game-2048-v2-anim/src/Main.elm
@@ -303,7 +303,7 @@ viewTile pos num anim =
             , style "border-radius" "3px"
             , class "w-100 h-100 flex justify-center items-center"
             , style "font-size" (String.fromInt (cellWidth // 2) ++ "px")
-            , class "code"
+            , class "code b"
             , style "color" (numToColor num |> first)
             , style "background-color" (numToColor num |> second)
             , case anim of
