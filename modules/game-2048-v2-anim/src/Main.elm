38d5fbcaa529bf626872f93f106e15a4107c18a5
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-14 14:32:05 +0530

    view polish
    the coding train: 2048 v2 with animation

--- modules/game-2048-v2-anim/src/Main.elm
+++ modules/game-2048-v2-anim/src/Main.elm
@@ -318,6 +318,49 @@ viewTile pos num anim =
         ]
 
 
+toBackgroundColor : Int -> String
+toBackgroundColor n =
+    case n of
+        2 ->
+            ""
+
+        4 ->
+            ""
+
+        8 ->
+            ""
+
+        16 ->
+            ""
+
+        32 ->
+            ""
+
+        64 ->
+            ""
+
+        128 ->
+            ""
+
+        256 ->
+            ""
+
+        512 ->
+            ""
+
+        1024 ->
+            ""
+
+        2048 ->
+            ""
+
+        4096 ->
+            ""
+
+        _ ->
+            ""
+
+
 cellWidth =
     100
 
