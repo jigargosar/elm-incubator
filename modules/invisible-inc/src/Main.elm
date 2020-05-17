4ba8769c42efa3951911e6224dc2a0539c80494d
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-17 16:05:04 +0530

    prototyping Invisible Inc.

---
+++ modules/invisible-inc/src/Main.elm
@@ -0,0 +1,71 @@
+module Main exposing (main)
+
+import Browser exposing (Document)
+import Html exposing (Html, text)
+
+
+
+-- Model
+
+
+type alias Model =
+    {}
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init () =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+
+-- View
+
+
+type alias DM =
+    Document Msg
+
+
+view : Model -> DM
+view _ =
+    Document "Invisible Inc."
+        [ text "Hello Invisible Inc." ]
+
+
+
+-- Main
+
+
+main : Program Flags Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
---
+++ modules/invisible-inc/src/index.js
@@ -0,0 +1,19 @@
+require('tachyons')
+require('./styles.css')
+
+const app = require('./Main.elm')['Elm']['Main']['init']({
+  node: document.getElementById('elm'),
+  flags: {
+    now: Date.now(),
+    window: { width: window.innerWidth, height: window.innerHeight },
+    cache: localStorage.getItem('cache') || '',
+  },
+})
+
+if (app.ports) {
+  if (app.ports.cache) {
+    app.ports.cache.subscribe(function(string) {
+      localStorage.setItem('cache', string)
+    })
+  }
+}
