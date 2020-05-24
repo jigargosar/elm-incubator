7af28c9cdc1dd16b1c655a51f6abf5c50163ab2b
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-24 17:46:36 +0530

    rouge-like

--- modules/rouge-like/src/Main.elm
+++ modules/rouge-like/src/Main.elm
@@ -4,27 +4,93 @@ module Main exposing (main)
 
 import Browser
 import Browser.Events
+import Dict exposing (Dict)
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
 import Json.Decode as JD
+import Tuple exposing (..)
 
 
 
 -- Grid
 
 
+type Tile
+    = Player
+    | Enemy
+    | Wall
+    | Empty
+
+
+tileFromChar : Char -> Tile
+tileFromChar char =
+    case char of
+        '#' ->
+            Wall
+
+        '3' ->
+            Player
+
+        'e' ->
+            Enemy
+
+        _ ->
+            Empty
+
+
+tileToChar : Tile -> Char
+tileToChar tile =
+    case tile of
+        Player ->
+            '3'
+
+        Enemy ->
+            'e'
+
+        Wall ->
+            '#'
+
+        Empty ->
+            '.'
+
+
 type Grid
-    = Grid {}
+    = Grid (Dict ( Int, Int ) Tile)
 
 
 gridInit : Grid
 gridInit =
-    Grid {}
+    [ "#..."
+    , ".#.e"
+    , "e..."
+    , "...3"
+    ]
+        |> List.map (String.toList >> List.map tileFromChar)
+        |> List.indexedMap (\r -> List.indexedMap (\c -> pair ( r, c )))
+        |> List.concat
+        |> Dict.fromList
+        |> Grid
 
 
-gridToList : Grid -> List String
-gridToList (Grid g) =
-    []
+gridToRows : Grid -> List (List Tile)
+gridToRows (Grid d) =
+    Dict.toList d
+        |> groupByRow
+
+
+groupByRow : List ( ( Int, Int ), a ) -> List (List a)
+groupByRow =
+    let
+        reducer ( ( r, c ), a ) =
+            Dict.update r
+                (Maybe.map (Dict.insert c a)
+                    >> Maybe.withDefault (Dict.singleton c a)
+                    >> Just
+                )
+    in
+    List.foldl reducer Dict.empty
+        >> Dict.values
+        >> List.map Dict.values
 
 
 gridMoveLeft : Grid -> Grid
@@ -102,7 +168,8 @@ view model =
     div [ class "measure center" ]
         [ div [ class "code f1" ]
             (model
-                |> gridToList
+                |> gridToRows
+                |> List.map (List.map tileToChar >> String.fromList)
                 |> List.map (\s -> div [] [ text s ])
             )
         ]
