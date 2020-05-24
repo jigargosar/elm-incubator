ed82325110ff506af46daa697add75272dd52393
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-24 17:57:51 +0530

    rouge-like

--- modules/rouge-like/src/Main.elm
+++ modules/rouge-like/src/Main.elm
@@ -1,18 +1,17 @@
 module Main exposing (main)
 
--- Browser.Element Scaffold
-
 import Browser
 import Browser.Events
 import Dict exposing (Dict)
 import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
 import Json.Decode as JD
+import Set exposing (Set)
 import Tuple exposing (..)
 
 
 
--- Grid
+-- Tile
 
 
 type Tile
@@ -54,8 +53,50 @@ tileToChar tile =
             '.'
 
 
+
+-- Grid
+
+
+type alias GridDict =
+    Dict ( Int, Int ) Tile
+
+
+type alias GridRecord =
+    { player : ( Int, Int )
+    , enemies : Set ( Int, Int )
+    , walls : Set ( Int, Int )
+    , empty : Set ( Int, Int )
+    }
+
+
+toGridRecord : GridDict -> GridRecord
+toGridRecord =
+    let
+        reducer ( position, t ) gr =
+            case t of
+                Player ->
+                    { gr | player = position }
+
+                Enemy ->
+                    { gr | enemies = Set.insert position gr.enemies }
+
+                Wall ->
+                    { gr | walls = Set.insert position gr.walls }
+
+                Empty ->
+                    { gr | empty = Set.insert position gr.empty }
+    in
+    Dict.toList
+        >> List.foldl reducer
+            { player = ( 0, 0 )
+            , enemies = Set.empty
+            , walls = Set.empty
+            , empty = Set.empty
+            }
+
+
 type Grid
-    = Grid (Dict ( Int, Int ) Tile)
+    = Grid GridDict
 
 
 gridInit : Grid
