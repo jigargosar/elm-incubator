9edfa80199b7f1aa06033cce1a97a297144ea511
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-24 17:12:10 +0530

    rouge-like

--- modules/rouge-like/src/Main.elm
+++ modules/rouge-like/src/Main.elm
@@ -1,21 +1,78 @@
 module Main exposing (main)
 
-import Html exposing (div, text)
+-- Browser.Element Scaffold
+
+import Browser
+import Html exposing (Html, div, text)
 import Html.Attributes exposing (class)
 
 
-main =
-    view
+
+-- Model
 
 
-view =
+type alias Model =
+    List String
+
+
+type alias Flags =
+    ()
+
+
+init : Flags -> ( Model, Cmd Msg )
+init _ =
+    ( [ "#..."
+      , ".#.e"
+      , "e..."
+      , "...3"
+      ]
+    , Cmd.none
+    )
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update message model =
+    case message of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.batch []
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
     div [ class "measure center" ]
         [ div [ class "code f1" ]
-            ([ "#..."
-             , ".#.e"
-             , "e..."
-             , "...3"
-             ]
+            (model
                 |> List.map (\s -> div [] [ text s ])
             )
         ]
+
+
+
+-- Main
+
+
+main : Program Flags Model Msg
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
