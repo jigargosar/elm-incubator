7415823ba7285b599a6a566300932fcae8565450
Jigar Gosar <jigar.gosar@gmail.com>
2020-05-30 19:08:38 +0530

    rouge-like

--- modules/rouge-like/src/Index.elm
+++ modules/rouge-like/src/Index.elm
@@ -6,6 +6,12 @@ range len =
     List.range 0 (len - 1)
 
 
+range2 : Int -> Int -> List (List ( Int, Int ))
+range2 l1 l2 =
+    range l1
+        |> List.map (\i1 -> range l2 |> List.map (Tuple.pair i1))
+
+
 memberOf : Int -> Int -> Bool
 memberOf len x =
     clamp 0 (len - 1) x == x
