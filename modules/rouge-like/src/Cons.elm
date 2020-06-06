5ee3968935f52abea6361cd01fab4071284d943f
Jigar Gosar <jigar.gosar@gmail.com>
2020-06-06 15:33:47 +0530

    rouge-like

--- modules/rouge-like/src/Cons.elm
+++ modules/rouge-like/src/Cons.elm
@@ -13,9 +13,53 @@ init =
     Tuple.pair
 
 
+singleton : a -> Cons a
+singleton x =
+    init x []
+
+
+push : a -> Cons a -> Cons a
+push x cons =
+    ( x, toList cons )
+
+
 fromList : List a -> Maybe (Cons a)
 fromList =
     List.uncons
+
+
+append : Cons a -> Cons a -> Cons a
+append c d =
+    let
+        step x xs =
+            init x <| toList xs
+    in
+    foldr step d c
+
+
+foldr : (a -> b -> b) -> b -> Cons a -> b
+foldr f x =
+    toList >> List.foldr f x
+
+
+foldl : (a -> b -> b) -> b -> Cons a -> b
+foldl f x =
+    toList >> List.foldl f x
+
+
+appendToList : List a -> Cons a -> Cons a
+appendToList l d =
+    case fromList l of
+        Nothing ->
+            d
+
+        Just c ->
+            append c d
+
+
+reverse : Cons a -> Cons a
+reverse ( first, rest ) =
+    appendToList (List.reverse rest) <| singleton first
 
 
 map : (a -> b) -> Cons a -> Cons b
@@ -26,3 +70,24 @@ map f =
 toList : Cons a -> List a
 toList =
     uncurry (::)
+
+
+mapAccuml : (a -> b -> ( a, c )) -> a -> Cons b -> ( a, Cons c )
+mapAccuml f acc0 ( x0, xs ) =
+    let
+        ( acc1, y0 ) =
+            f acc0 x0
+
+        ( accFinal, generatedCons ) =
+            List.foldl
+                (\x ( acc, yCons ) ->
+                    let
+                        ( nAcc, y ) =
+                            f acc x
+                    in
+                    ( nAcc, push y yCons )
+                )
+                ( acc1, singleton y0 )
+                xs
+    in
+    ( accFinal, reverse generatedCons )
